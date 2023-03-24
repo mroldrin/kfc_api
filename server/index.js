73f308da66d484d212242fd46f8214e020f6a44d
Marcelo Roldrin <ext_maroldri@mercadolivre.com>
2023-03-24 14:15:25 -0300

    alteracoes

--- server/index.js
+++ server/index.js
@@ -1,8 +1,12 @@
 import jsonServer from "json-server"
 import cors from "cors"
+import * as url from 'url';
+import path from "path";
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 
 const server = jsonServer.create()
-const router = jsonServer.router('./db/db.json')
+const router = jsonServer.router(path.join(__dirname,"..","db","db.json"))
 const middlewares = jsonServer.defaults()
 
 const PORT = 4004
